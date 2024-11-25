d5b607dcf128898ef1903bc9730caa48ce92205f
BryanWV <wilchesbryan@gmail.com>
2024-11-25 16:22:09 -0500

    Automatic Blazor WASM Deployment to GitHub Pages

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,15 @@ OUTPUT_DIR="docs" #Folder that GitHub Pages allows deployment
 BASE_HREF="Music-portfolio-blazor/"
 INDEX_FILE="docs/index.html"
 
-dotnet publish --configuration Release --output $OUTPUT_DIR
-mv docs/wwwroot/* docs/
-rm -r docs/wwwroot
+dotnet publish --configuration Release
+cp -r bin/Release/net8.0/publish/wwwroot/* docs/
 
 #Update <base> href in index.html
 
 if [ -f $INDEX_FILE ];
 then
 	echo "index file exists"
-	sed -i 's/base href="\/"/base href="Music-portfolio-blazor"/g' docs/index.html
+	sed -i 's/base href="\/"/base href="julianmusic"/g' docs/index.html
 	echo "Updated <base href> in $INDEX_FILE"
 else
 	echo "index.html file not found in $OUTPUT_DIR"
@@ -29,6 +28,7 @@ cd docs/
 touch 404.html
 cp index.html 404.html
 touch .nojekyll
+cd ..
 
 git add .
 git commit -m "Automatic Blazor WASM Deployment to GitHub Pages"
--- obj/Debug/net8.0/julianmusic.AssemblyInfo.cs
+++ obj/Debug/net8.0/julianmusic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("julianmusic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fca174542b2ea25b732d1536a97b083fccca7b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a030488048c1506e8c8d8b32f7d2555a8f7fa0ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("julianmusic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("julianmusic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
