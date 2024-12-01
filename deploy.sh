6f4c7f33f9aa9c572816544feea4e3fbb27bed2f
BryanWV <wilchesbryan@gmail.com>
2024-12-01 14:13:22 -0500

    Automated Blazor WASM Deployment to GitHub Pages in

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,17 @@ mkdir docs
 
 # Variables
 OUTPUT_DIR="docs" #Folder that GitHub Pages allows deployment
-BASE_HREF="Music-portfolio-blazor/"
 INDEX_FILE="docs/index.html"
-
+TIMESTAMP =$(date -u +"%Y-%m-%d %T %Z")
 dotnet publish --configuration Release
-cp -r bin/Release/net8.0/publish/wwwroot/* docs/
+cp -r bin/Release/net8.0/publish/wwwroot/* $OUTPUT_DIR/
 
 #Update <base> href in index.html
 
 if [ -f $INDEX_FILE ];
 then
 	echo "index file exists"
-	sed -i 's/base href="\/"/base href="julianmusic"/g' docs/index.html
+	sed -i 's/base href="\/"/base href="julianmusic"/g' $INDEX_FILE
 	echo "Updated <base href> in $INDEX_FILE"
 else
 	echo "index.html file not found in $OUTPUT_DIR"
@@ -31,5 +30,5 @@ touch .nojekyll
 cd ..
 
 git add .
-git commit -m "Automatic Blazor WASM Deployment to GitHub Pages"
+git commit -m "Automated Blazor WASM Deployment to GitHub Pages in $TIMESTAMP"
 git push -u origin main
