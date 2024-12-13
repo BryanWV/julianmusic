5eb2e73ce9c1a6b3e7940035e2b1bcbbe71f49dd
BryanWV <wilchesbryan@gmail.com>
2024-12-13 17:59:49 -0500

    Navbar menu collapsing solution added

--- obj/Debug/net8.0/julianmusic.AssemblyInfo.cs
+++ obj/Debug/net8.0/julianmusic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("julianmusic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e50fbb1d441d70e249e7f94d0ecc833a3adadd59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8c49e8657cbcf82ae4305007569bf6144011847")]
 [assembly: System.Reflection.AssemblyProductAttribute("julianmusic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("julianmusic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/site.js
+++
@@ -1,9 +0,0 @@
-myApp.collapseMenu = function () {
-    let navbar = document.getElementById("navbarText");
-
-    if (navbar != null) {
-        bootstrap.Collapse.getInstance(navbar).hide();
-
-    }
-
-}
