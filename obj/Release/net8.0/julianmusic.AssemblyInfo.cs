8149fd4c7f633dcc9b1f9ff339c7bc810346f536
BryanWV <wilchesbryan@gmail.com>
2025-01-14 12:18:22 -0500

    Automated Blazor WASM Deployment to GitHub Pages in 2025-01-14 17:17:03 UTC

--- obj/Release/net8.0/julianmusic.AssemblyInfo.cs
+++ obj/Release/net8.0/julianmusic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("julianmusic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+527cafe625b60219d13b710ee37d7d1a3a0b4a93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+907a39a1e27b8e92d576585b05d565a4e4aab9b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("julianmusic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("julianmusic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
